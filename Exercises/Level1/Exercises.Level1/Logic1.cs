a4b3e9b9704eacd3428144f411d303081bd6aa7e
kristdina <kristdina@gmail.com>
2021-09-22 20:47:07 +0300

    uzd 1

--- Exercises/Level1/Exercises.Level1/Logic1.cs
+++ Exercises/Level1/Exercises.Level1/Logic1.cs
@@ -19,7 +19,16 @@ namespace Exercises.Level1
         /// </summary>
         public bool CigarParty(int cigars, bool isWeekend)
         {
-            throw new NotImplementedException();
+            if (cigars >= 40 && cigars <= 60)
+            {
+                return true;
+            }
+
+            if (cigars >= 40 && isWeekend)
+            {
+                return true;
+            }
+            return false;
         }
 
         /// <summary>
